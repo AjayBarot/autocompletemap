644c81c994e818243414f87350d0a0316ed74a9d
AjayBarot <ajubarot@gmail.com>
2014-03-20 15:18:49 +0530

    heroku

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Googlemapsauto::Application.config.secret_key_base = '1e0c797ef579cee424b59b1dffba2e353a953035fd439339fd971105d5dcd79719ccc73f9407ef28ef48ad1aa80eea730b6e58acd8d50bfd2784bde883744cf8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Googlemapsauto::Application.config.session_store :cookie_store, key: '_googlemapsauto_session'
